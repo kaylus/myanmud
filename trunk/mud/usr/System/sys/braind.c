0cf3629bd6fb6ee00decd23d48e27dd5c2d8121a
hambone20@gmail.com <hambone20@gmail.com@9db1a9f7-661f-0410-a025-fb7d5554b9fe>
2012-03-27 02:11:21 +0000

    Changed braind this_player over to tls.

--- trunk/mud/usr/System/sys/braind.c
+++ trunk/mud/usr/System/sys/braind.c
@@ -4,7 +4,7 @@
 
 inherit tls API_TLS;
 
-object this_player;
+/*object this_player;*/
 
 static void create() {
     tls::create();
@@ -13,13 +13,14 @@ static void create() {
 
 /* TODO: check to make these calls are from a reliable source */
 void set_this_player(object player) {
-    LOGD->log("Set player to "+object_name(player), "braind");
-    /*set_tlvar(0, player);*/
-    this_player = player;
+    /*LOGD->log("Set tls player to "+object_name(player), "braind");*/
+    set_tlvar(0, player);
+    /*this_player = player;*/
 }
 
 /* return the current player */
 object this_player() {
-    /*return get_tlvar(0);*/
-    return this_player ? this_player : this_user();
+    /*LOGD->log("queried this player for tls", "braind");*/
+    return get_tlvar(0);
+    /*return this_player ? this_player : this_user();*/
 }
